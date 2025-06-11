21faa7c817fc1c076a229784a2bcc71afcc997b8
RoHaNaLpHaCoDeR <rohan.arora1812@gmail.com>
2025-06-12 01:18:26 +0530

    .

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from RealESRGAN import RealESRGAN
 
 def main() -> int:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = RealESRGAN(device, scale=4)
-    model.load_weights('weights/RealESRGAN_x4.pth', download=True)
+    model = RealESRGAN(device, scale=8)
+    model.load_weights('weights/RealESRGAN_x8.pth', download=False)
     for i, image in enumerate(os.listdir("inputs")):
         image = Image.open(f"inputs/{image}").convert('RGB')
         sr_image = model.predict(image)
